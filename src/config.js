7f7a7a9bb46e9ffb0d91691eb6de1cc28d37bec0
keysolbo <keysolutionsbo@gmail.com>
2023-07-02 23:35:30 -0400

    Se configuro el puerto para el server a 3020

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 import { config } from 'dotenv';
 config();
 
-export const PORT = process.env.PORT || 3000
+export const PORT = process.env.PORT || 3020
 export const DB_HOST = process.env.DB_HOST || '154.12.232.98'
 export const DB_USER = process.env.DB_USER || 'bot3app'
 export const DB_PASSWORD = process.env.DB_PASSWORD || 's1WpNkyt1!HC62'
